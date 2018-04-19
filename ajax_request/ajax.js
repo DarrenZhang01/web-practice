0eb96f7528b8a33ba50ef7f07a23c09bb8833108
Darren <18633059886@163.com>
2018-04-19 15:55:01 -0400

    Add readable stream operation

--- ajax_request/ajax.js
+++ ajax_request/ajax.js
@@ -2,11 +2,52 @@
 "use strict"
 /* There is a browser side js file for sending ajax request, and
     this server side js file is for receiving information from the ajax */
-let http = require("http");
+// let http = require("http");
+// let fs = require('fs');
+//
+// function server (request, response) {
+//     console.log("The request is received!");
+//     console.log("The url is: " + request.url);
+//     if (request.url == '/test/ajax') {
+//         console.log("The ajax request is received!");
+//         response.send("Successful!");
+//         response.end();
+//     }
+// }
+//
+// let app = http.createServer(server);
+// app.listen(4001);
 
-function server (request, response) {
+let express = require("express");
+let app = new express();
+var bodyParser = require('body-parser');
 
-}
+app.use(bodyParser.text({ type: 'text/html' }));
+/* Access Allow Control Origin (CORS) Set Up */
+app.use(function (req, res, next) {
 
-let app = http.createServer(server);
+	res.setHeader('Access-Control-Allow-Origin', 'http://www.darrenzhang.com');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+
+	next();
+});
+
+app.post('/test/ajax', function (request, response) {
+    console.log("The request is received!");
+    console.log("The url is: " + request.url);
+    // console.log("The request data: " + request.body.data);
+    let body = [];
+    request.on('data', function (chunk) {
+        body.push(chunk);
+    });
+    request.on('end', function () {
+        console.log("The body: " + Buffer.concat(body).toString());
+    });
+    response.status(200);
+    response.send("Successful!");
+    response.end();
+});
+
 app.listen(4001);
