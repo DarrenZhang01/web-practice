a594ffc694ca31ad53fa327a1c224e05c6fd3401
Darren <18633059886@163.com>
2018-04-19 10:33:37 -0400

    Check file existence, open and close file

--- fs/fs_operation.js
+++ fs/fs_operation.js
@@ -2,9 +2,32 @@
 "use strict"
 let fs = require('fs');
 
-fs.rename("rename", "newname", function(err) {
-    if (err) {
-        throw err;
+// Rename the file rename into new name
+fs.rename("rename", "newname", function(error) {
+    if (error) {
+        throw error;
     }
     console.log("renaming successfully!");
+});
+
+// Check the existence of the file
+fs.stat("newname", function (error, status) {
+    if (error) {
+        throw error;
+    }
+    console.log(`The file exists: ${JSON.stringify(status)}`);
+});
+
+// Open the file, read it and close it.
+fs.open('newname', 'r', function (error, fd) {
+    if (error) {
+        throw error;
+    }
+    console.log("The file is opened successfully!");
+    fs.close(fd, function (err) {
+        if (err) {
+            throw err;
+        }
+        console.log("The file is closed successfully!");
+    })
 });
