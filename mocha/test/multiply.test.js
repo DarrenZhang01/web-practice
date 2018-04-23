f4f05a6b49fdaa4739db91629e7b9d428a596353
Darren <18633059886@163.com>
2018-04-23 12:25:07 -0400

    Add test cases

--- mocha/test/multiply.test.js
+++ mocha/test/multiply.test.js
@@ -10,4 +10,10 @@ describe ("Testing for function multiply!", function () {
     it ("3 times 4 should equal 12 ", function () {
         expect(multiply(3, 4)).to.be.equal(12);
     });
+    it ("2 times 4 should equal 8 ", function () {
+        expect(multiply(2, 4)).to.be.equal(8);
+    });
+    it ("1 times 10 should equal 10 ", function () {
+        expect(multiply(1, 10)).to.be.equal(10);
+    });
 });
