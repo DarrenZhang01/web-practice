1b035b3a68742094d4353bfe0db3dd3a4e8fb611
Darren <18633059886@163.com>
2018-05-02 11:40:13 -0400

    the simplest php

---
+++ PHP/example.php
@@ -0,0 +1,13 @@
+<!-- This file is for the use of PHP -->
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>PHP demo</title>
+    </head>
+    <body>
+        <?php
+        echo "Hello World!";
+        ?>
+    </body>
+</html>
