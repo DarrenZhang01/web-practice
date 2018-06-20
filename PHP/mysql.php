6208177a9600d42ebb2bd12d53043bd17d57d4f5
Darren <18633059886@163.com>
2018-06-20 10:09:57 -0400

    create database using PHP

--- PHP/mysql.php
+++ PHP/mysql.php
@@ -1,3 +1,5 @@
+<!-- Use PHP to connect to the MySQL database -->
+<!-- Reference from "http://www.runoob.com/php/php-mysql-create.html" -->
 <?php
 $servername = "localhost";
 $username = "root";
@@ -5,9 +7,17 @@ $username = "root";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=myDB", $username);
     echo "connect successfully\n";
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // use exec to execute the sql statement
+    $sql = "create database testPDO;";
+    $conn->exec($sql);
+    echo "database 'testPDO' created successfully!\n";
+
     echo $conn = null;
     echo "connection is closed!\n";
 }
+
 catch(PDOException $e)
 {
     echo $e->getMessage();
