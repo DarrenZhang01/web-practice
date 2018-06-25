c0efe7051274dd784483514f2a4d2ce7eb7d482b
Darren <18633059886@163.com>
2018-06-25 14:56:26 -0400

    try to get back result from the server

--- PHP/mysql.php
+++ PHP/mysql.php
@@ -10,13 +10,25 @@ try {
 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // use exec to execute the sql statement
-    $sql = "create database testPDO;";
+    $sql = "use testPDO;";
     $conn->exec($sql);
-    echo "database 'testPDO' created successfully!\n";
+    echo "database changed successfully to testPDO!\n";
 
+    $sql = "select * from user_info;";
+    $result = $conn->query($sql);
+    // var_dump($result);
+    // echo "The result of the execution is " . $result . "\n";
+    if ($result->num_rows > 0) {
+        // print data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "name: " . $row['name'] . "\n";
+        }
+    } else {
+        echo "0 result" . "\n";
+    }
     echo $conn = null;
     echo "connection is closed!\n";
-    
+
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
