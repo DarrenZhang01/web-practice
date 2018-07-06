6daf2482e39df530f4344601956cecba90bdcea0
Darren <18633059886@163.com>
2018-07-06 14:39:57 -0400

    get information back from the MySQL database

---
+++ PHP/get_from_sql.php
@@ -0,0 +1,34 @@
+<!-- Use PHP to connect to the MySQL database -->
+<!-- Reference from "http://www.runoob.com/php/php-mysql-create.html" -->
+<?php
+$servername = "localhost";
+$username = "root";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username);
+    echo "connect successfully\n";
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // use exec to execute the sql statement
+    $sql = "use testPDO;";
+    $conn->exec($sql);
+    echo "database changed successfully to testPDO!\n";
+
+    $sql = "select * from user_info;";
+
+    // $stmt = $conn->prepare($sql);
+    //
+    // $stmt->execute();
+    //
+    // $fetched = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach($conn->query($sql) as $row) {
+        echo $row[0] . "\n";
+    }
+
+    echo $conn = null;
+    echo "connection is closed!\n";
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+?>
